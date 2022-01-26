cf257a250f3b232c80320c289cce112306dbce5d
Xzi <nehetejay@gmail.com>
2022-01-26 17:17:28 +0530

    updated

--- talkdota/settings.py
+++ talkdota/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,4 +140,8 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_heroku.settings(locals())
